faca37138274ba0e56ae5bbf132f46619accf409
Liliana <molilis.pr95@live.com.mx>
2019-02-01 12:05:54 -0600

    design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FooterModule } from './components/footer/footer.module';
+import { NavbarModule } from './components/navbar/navbar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    FooterModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
